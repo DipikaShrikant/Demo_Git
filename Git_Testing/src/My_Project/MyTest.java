a93d28a17f2ef6367752de73962d91d7e9f48abe
DipikaShrikant <165634072+DipikaShrikant@users.noreply.github.com>
2024-04-01 12:45:20 +0530

    Update MyTest.java

--- Git_Testing/src/My_Project/MyTest.java
+++ Git_Testing/src/My_Project/MyTest.java
@@ -4,6 +4,9 @@ public class MyTest {
 public void Sum() {
 	System.out.println(10+20);
 }
+	public void Sum1() {
+	System.out.println(10+20);
+}
 	public static void main(String[] args) {
 		System.out.println("This is Git");
 	}
